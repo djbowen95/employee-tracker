5025be8fb62a8becd6066bf799daa824f437649b
Daniel Bowen <djbowen95@gmail.com>
2022-04-01 17:46:05 +0100

    Display department name when users query to view roles

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ function selectTask() {
         message: "What would you like to do?",
         choices: [
           {
-            name: "View All Employees",
-            value: "viewEmployees",
-          },
-          {
             name: "View All Departments",
             value: "viewDepartments",
           },
@@ -28,31 +24,27 @@ function selectTask() {
             name: "View All Roles",
             value: "viewRoles",
           },
+          {
+            name: "View All Employees",
+            value: "viewEmployees",
+          },
         ],
       },
     ])
     .then((res) => {
       const choice = res.choice;
       switch (choice) {
-        case "viewEmployees":
-          viewAllEmployees();
-          break;
         case "viewDepartments":
           viewAllDepartments();
           break;
         case "viewRoles":
           viewAllRoles();
           break;
+        case "viewEmployees":
+          viewAllEmployees();
+          break;
       }
     });
-}
-
-function viewAllEmployees() {
-  db.query("SELECT * FROM employee", (err, data) => {
-    if (err) throw err;
-    console.table(data);
-    selectTask();
-  });
 }
 
 function viewAllDepartments() {
@@ -63,12 +55,34 @@ function viewAllDepartments() {
   });
 }
 
+//THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
 function viewAllRoles() {
-  db.query("SELECT * FROM roles", (err, data) => {
+  db.query("SELECT roles.id, roles.title, department.department_name AS department FROM roles JOIN department ON roles.department_id = department.id", (err, data) => {
     if (err) throw err;
     console.table(data);
     selectTask();
   });
 }
+
+function viewAllEmployees() {
+  db.query("SELECT * FROM employee", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    selectTask();
+  });
+}
+
+// Add a department
+// Add a role
+// Add an employee
+// Update an employee's role
+// BONUS: Update employee's manager
+// BONUS: View employee by manager
+// BONUS: View employees by department
+// BONUS: Delete department
+// BONUS: Delete role
+// BONUS: Delete employee
+// BONUS: View 'utilised budget', ie salary for everyone in the department
+// Function to Quit Application
 
 start();
