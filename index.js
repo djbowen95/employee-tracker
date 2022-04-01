cdaebe6442ce8a44f6337820a67d39127e08aac2
Daniel Bowen <djbowen95@gmail.com>
2022-04-01 17:20:00 +0100

    Allow user query to view all roles

--- index.js
+++ index.js
@@ -3,40 +3,49 @@ const { end } = require("./config/connection");
 const db = require("./config/connection");
 require("console.table");
 
-function start () {
-    console.log("Welcome to the Employee Tracker.");
-    selectTask();
+function start() {
+  console.log("Welcome to the Employee Tracker.");
+  selectTask();
 }
 
-function selectTask () {
-    inquirer.prompt([
-        {
-            type: "list", 
-            name: "choice", 
-            message: "What would you like to do?", 
-            choices: [
-                {
-                    name: "View All Employees", 
-                    value: "viewEmployees"
-                },
-                {
-                    name: "View All Departments",
-                    value: "viewDepartments"
-                }
-            ]
-        }
-    ]).then(res => {
-            const choice = res.choice;
-            switch (choice) {
-                case "viewEmployees":
-                    viewAllEmployees();
-                    break;
-                case "viewDepartments":
-                    viewAllDepartments();
-                    break;
-                }
-            })
-        };
+function selectTask() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          {
+            name: "View All Employees",
+            value: "viewEmployees",
+          },
+          {
+            name: "View All Departments",
+            value: "viewDepartments",
+          },
+          {
+            name: "View All Roles",
+            value: "viewRoles",
+          },
+        ],
+      },
+    ])
+    .then((res) => {
+      const choice = res.choice;
+      switch (choice) {
+        case "viewEmployees":
+          viewAllEmployees();
+          break;
+        case "viewDepartments":
+          viewAllDepartments();
+          break;
+        case "viewRoles":
+          viewAllRoles();
+          break;
+      }
+    });
+}
 
 function viewAllEmployees() {
   db.query("SELECT * FROM employee", (err, data) => {
@@ -44,14 +53,22 @@ function viewAllEmployees() {
     console.table(data);
     selectTask();
   });
-};
+}
 
 function viewAllDepartments() {
-    db.query("SELECT * FROM department", (err, data) => {
-      if (err) throw err;
-      console.table(data);
-      selectTask();
-    });
-  }
+  db.query("SELECT * FROM department", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    selectTask();
+  });
+}
+
+function viewAllRoles() {
+  db.query("SELECT * FROM roles", (err, data) => {
+    if (err) throw err;
+    console.table(data);
+    selectTask();
+  });
+}
 
 start();
