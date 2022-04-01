881235508961833052bc26b35cdab531b3784cb1
Daniel Bowen <djbowen95@gmail.com>
2022-04-01 17:10:35 +0100

    Ask user to select a task in the terminal

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const inquirer = require("inquirer");
+const { end } = require("./config/connection");
 const db = require("./config/connection");
 require("console.table");
 
-function viewAllDepartments() {
-  db.query("SELECT * FROM department", (err, data) => {
-    if (err) throw err;
-    console.table(data);
-  });
+function start () {
+    console.log("Welcome to the Employee Tracker.");
+    selectTask();
 }
+
+function selectTask () {
+    inquirer.prompt([
+        {
+            type: "list", 
+            name: "choice", 
+            message: "What would you like to do?", 
+            choices: [
+                {
+                    name: "View All Employees", 
+                    value: "viewEmployees"
+                }
+            ]
+        }]).then(res => {
+            const choice = res.choice;
+            switch (choice) {
+                case "viewEmployees":
+                    viewAllEmployees();
+                    break;
+            }
+        })                
+    };
+
+start();
