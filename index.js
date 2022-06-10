67808323fd2a0c55f208f042d96183d7f76efc4e
Daniel Bowen <djbowen95@gmail.com>
2022-06-10 15:03:34 +0100

    User can select a department and create a new role

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { createPromptModule } = require("inquirer");
 const inquirer = require("inquirer");
 const db = require("./config/connection");
 require("console.table");
@@ -9,7 +10,7 @@ function start() {
 }
 
 // SELECT TASK: Asks user to choose which task they would like to carry out.
-// Uses inquirer to prompt the user in the terminal; then switch/case to run the relevant function. 
+// Uses inquirer to prompt the user in the terminal; then switch/case to run the relevant function.
 function selectTask() {
   inquirer
     .prompt([
@@ -34,6 +35,10 @@ function selectTask() {
             name: "Create New Department",
             value: "newDepartment",
           },
+          {
+            name: "Create New Role",
+            value: "newRole",
+          },
         ],
       },
     ])
@@ -52,6 +57,9 @@ function selectTask() {
         case "newDepartment":
           createDepartment();
           break;
+        case "newRole":
+          createRole();
+          break;
       }
     });
 }
@@ -66,7 +74,7 @@ function viewAllDepartments() {
 }
 
 // VIEW ALL ROLES: Table logs to console displaying all roles across departments.
-// Query references primary key of department id to return the department name. 
+// Query references primary key of department id to return the department name.
 function viewAllRoles() {
   db.query(
     "SELECT roles.id, title, department.department_name AS department, salary FROM roles JOIN department ON roles.department_id = department.id",
@@ -79,40 +87,99 @@ function viewAllRoles() {
 }
 
 // VIEW ALL EMPLOYEES: Table logs to console displaying all employee data.
-// Query joins on department, roles and manager data to build full employee profile. 
+// Query joins on department, roles and manager data to build full employee profile.
 function viewAllEmployees() {
-    db.query("SELECT employee.id, employee.first_name, roles.title AS job_title, department.department_name AS department, managers.first_name AS manager FROM employee JOIN employee AS managers ON employee.manager_id = managers.id JOIN roles ON employee.role_id = roles.id JOIN department ON roles.department_id = department.id",
+  db.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, roles.title AS job_title, department.department_name AS department FROM employee LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id",
+    // "SELECT employee.id, employee.first_name, employee.last_name, roles.title AS job_title, department.department_name AS department, managers.first_name AS manager FROM employee INNER JOIN employee AS managers ON employee.manager_id = managers.id LEFT JOIN roles ON employee.role_id = roles.id LEFT JOIN department ON roles.department_id = department.id",
+    //    "SELECT employee.id, employee.first_name, roles.title AS job_title, department.department_name AS department, managers.first_name AS manager FROM employee JOIN employee AS managers ON employee.manager_id = managers.id JOIN roles ON employee.role_id = roles.id JOIN department ON roles.department_id = department.id",
     (err, data) => {
-    if (err) throw err;
-    console.table(data);
-    selectTask();
-  });
+      if (err) throw err;
+      console.table(data);
+      selectTask();
+    }
+  );
 }
 // BUG: Currently only returns employees with a manager; when manager is set to 'Null', this query does not return a row in the table.
 
 // CREATE NEW DEPARTMENT: Asks user to name a new department.
-// Query inserts the user's input as a new entry in the department database. 
+// Query inserts the user's input as a new entry in the department database.
 function createDepartment() {
-  inquirer.prompt([
-    {
-      name: "name",
-      message: "What would you like to call your new department?",
-    }
-  ]).then(res => {
-    const name = res.name;
-    // Function to validate / make sure its VARCHAR 30 here would be useful.
-    db.query(`INSERT INTO department (department_name) VALUES ("${name}")`,
-    (err) => {
-      if (err) throw err;
-      console.log(`Department ${name} created.`);
-      selectTask();
-    })
-  })
-};
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        message: "What would you like to call your new department?",
+      },
+    ])
+    .then((res) => {
+      const name = res.name;
+      // Function to validate / make sure its VARCHAR 30 here would be useful.
+      db.query(
+        `INSERT INTO department (department_name) VALUES ("${name}")`,
+        (err) => {
+          if (err) throw err;
+          console.log(`Department ${name} created.`);
+          selectTask();
+        }
+      );
+    });
+}
 
 // Add a role
+function createRole() {
+  db.query("SELECT * FROM department", (err, res) => {
+    if (err) throw err;
+    let department = res.map((departments) => ({
+      name: departments.department_name,
+      value: departments.id,
+    }));
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What would you like to call your new role?",
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "Which department does your new role belong to?",
+          choices: department,
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "What is the expected salary for the new role?",
+        },
+      ])
+      .then((answers) => {
+        db.query("INSERT INTO roles SET ?", {
+          title: answers.name,
+          salary: answers.salary,
+          department_id: answers.department,
+        },
+        (err, res) => {
+          if (err) throw (err);
+          console.log(`${answers.name} was successfully added to the database.`);
+          selectTask();
+        }
+        );
+      });
+  });
+}
+
 // Add an employee
+// Create list of departments to display as choices.
+// Change database to auto_increment.
+
+
 // Update an employee's role
+// Query the employees - so there is a list of the employees
+// To update: query the roles, so they can choose the role
+// UPDATE employee SET ? WHERE ? 
+// [{role_id - answers (foreign key in employee)}, {id: answer}]
+
 // BONUS: Update employee's manager
 // BONUS: View employee by manager
 // BONUS: View employees by department
